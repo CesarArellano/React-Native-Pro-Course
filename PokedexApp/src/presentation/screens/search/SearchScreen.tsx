dd55674a19adf8cef0233c5ee6770ed6b84b01af
Cesar Arellano <raywayday@gmail.com>
2024-05-21 19:45:17 -0600

    show pokemon results on screen

--- PokedexApp/src/presentation/screens/search/SearchScreen.tsx
+++ PokedexApp/src/presentation/screens/search/SearchScreen.tsx
@@ -5,6 +5,9 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {ActivityIndicator, Text, TextInput} from 'react-native-paper';
 import {useQuery} from '@tanstack/react-query';
 import {getPokemonNamesWithId} from '../../../actions/pokemons/';
+import {FullScreenLoader} from '../../components/shared/FullScreenLoader';
+import {getPokemonsByIds} from '../../../actions/pokemons/get-pokemons-by-ids';
+import {PokemonCard} from '../../components/pokemons/PokemonCard';
 
 export const SearchScreen = () => {
   const {top} = useSafeAreaInsets();
@@ -34,6 +37,17 @@ export const SearchScreen = () => {
     );
   }, [term, pokemonNameList]);
 
+  const {isLoading: isLoadingPokemons, data: pokemons = []} = useQuery({
+    queryKey: ['pokemon', 'by', pokemonNameIdList],
+    queryFn: () =>
+      getPokemonsByIds(pokemonNameIdList.map(pokemon => pokemon.id)),
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (isLoading) {
+    return <FullScreenLoader />;
+  }
+
   return (
     <View style={[globalTheme.globalMargin, {paddingTop: top + 10}]}>
       <TextInput
@@ -44,16 +58,18 @@ export const SearchScreen = () => {
         onChangeText={value => setTerm(value)}
         value={term}
       />
-      {isLoading && <ActivityIndicator style={styles.activityIndicator} />}
-      <Text>{JSON.stringify(pokemonNameIdList, null, 2)}</Text>
+      {isLoadingPokemons && (
+        <ActivityIndicator style={styles.activityIndicator} />
+      )}
       <FlatList
-        data={pokemonNameList}
+        data={pokemons}
         keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
         ListHeaderComponent={HeaderText}
+        ListFooterComponent={<View style={styles.footerContainer} />}
         style={{paddingTop: top + 20}}
-        // onEndReached={() => fetchNextPage()}
-        renderItem={pokemon => <Text>{pokemon.item.id}</Text>}
-        showsVerticalScrollIndicator
+        renderItem={({item}) => <PokemonCard key={item.id} pokemon={item} />}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   );
@@ -73,5 +89,8 @@ const styles = StyleSheet.create({
   },
   activityIndicator: {
     top: 15,
+  },
+  footerContainer: {
+    height: 75,
   },
 });
