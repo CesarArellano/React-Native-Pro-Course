6e635b1649103716a268b33e238c2ac4514dab06
Cesar Arellano <raywayday@gmail.com>
2024-05-26 20:02:23 -0600

    design PermissionsScreen component

--- 09-MapsApp/src/MapsApp.tsx
+++ 09-MapsApp/src/MapsApp.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './presentation/navigation/StackNavigator';
+import {PaperProvider} from 'react-native-paper';
 
 export const MapsApp = () => {
   return (
--- 09-MapsApp/src/presentation/screens/permissions/PermissionsScreen.tsx
+++ 09-MapsApp/src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Button, Text} from 'react-native-paper';
+import {usePermissionStore} from '../../store/permissions/usePermissionStore';
 
 export const PermissionsScreen = () => {
+  const {locationStatus, requestLocationPermission} = usePermissionStore();
   return (
-    <View>
-      <Text>PermissionsScreen</Text>
+    <View style={styles.container}>
+      <Text>Enable location</Text>
+      <Button
+        mode="contained"
+        onPress={requestLocationPermission}
+        buttonColor="black">
+        Activate it
+      </Button>
+      <Text>Current status: {locationStatus}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 4,
+  },
+});
