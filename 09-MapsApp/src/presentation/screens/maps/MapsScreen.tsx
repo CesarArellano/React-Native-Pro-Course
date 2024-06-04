ab4f5def5528b026408538f384d7cb165aa3d32e
Cesar Arellano <raywayday@gmail.com>
2024-06-03 19:40:55 -0600

    add getCurrentLocation function and geolocation package

--- 09-MapsApp/src/presentation/screens/maps/MapsScreen.tsx
+++ 09-MapsApp/src/presentation/screens/maps/MapsScreen.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-// import {CustomMap} from '../../components/maps/Map';
+import {CustomMap} from '../../components/maps/Map';
+import {getCurrentLocation} from '../../../actions/location/location';
+import {Location} from '../../../infrastructure/interfaces/location';
 
 export const MapsScreen = () => {
+  getCurrentLocation().then(location => {
+    console.log({location});
+  });
+
   return <View style={styles.fullContainer}>{/* <CustomMap /> */}</View>;
 };
 
