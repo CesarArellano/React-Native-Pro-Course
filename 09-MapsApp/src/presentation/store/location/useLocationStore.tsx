a837a9e83830a750d52b3e9c13301498a8f3c597
Cesar Arellano <raywayday@gmail.com>
2024-06-03 23:07:22 -0600

    add tracking user feature

--- 09-MapsApp/src/presentation/store/location/useLocationStore.tsx
+++ 09-MapsApp/src/presentation/store/location/useLocationStore.tsx
@@ -1,17 +1,46 @@
 import {create} from 'zustand';
 import {Location} from '../../../infrastructure/interfaces/location';
-import {getCurrentLocation} from '../../../actions/location/location';
+import {
+  clearWatchLocation,
+  getCurrentLocation,
+  watchCurrentLocation,
+} from '../../../actions/location/location';
 
 interface LocationState {
+  watchId: number | null;
   lasKnownLocation: Location | null;
+  userLocations: Location[];
   getLocation: () => Promise<Location | null>;
+  watchLocation: () => void;
+  clearWatchLocation: () => void;
 }
 
-export const useLocationStore = create<LocationState>()(set => ({
+export const useLocationStore = create<LocationState>()((set, get) => ({
+  watchId: null,
   lasKnownLocation: null,
+  userLocations: [],
   getLocation: async () => {
     const location = await getCurrentLocation();
     set({lasKnownLocation: location});
     return location;
+  },
+  watchLocation: () => {
+    const watchId = get().watchId;
+    if (watchId !== null) {
+      get().clearWatchLocation();
+    }
+    const id = watchCurrentLocation(location => {
+      set({
+        lasKnownLocation: location,
+        userLocations: [...get().userLocations, location],
+      });
+    });
+    set({watchId: id});
+  },
+  clearWatchLocation: () => {
+    const watchId = get().watchId;
+    if (watchId !== null) {
+      clearWatchLocation(watchId);
+    }
   },
 }));
