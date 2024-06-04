61f0171bed5960fbe0aff30a7e52a90d2687cf8b
Cesar Arellano <raywayday@gmail.com>
2024-06-03 19:32:19 -0600

    show user location and define location interface for our maps app

--- 09-MapsApp/src/presentation/components/maps/Map.tsx
+++ 09-MapsApp/src/presentation/components/maps/Map.tsx
@@ -1,16 +1,26 @@
 import {StyleSheet} from 'react-native';
 import React from 'react';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {Location} from '../../../infrastructure/interfaces/location';
 
-export const CustomMap = () => {
+interface Props {
+  showsUserLocation: boolean;
+  initialLocation: Location;
+}
+
+export const CustomMap = ({
+  showsUserLocation = true,
+  initialLocation,
+}: Props) => {
   return (
     <>
       <MapView
+        showsUserLocation={showsUserLocation}
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={styles.map}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialLocation.latitude,
+          longitude: initialLocation.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
--- 09-MapsApp/src/presentation/screens/maps/MapsScreen.tsx
+++ 09-MapsApp/src/presentation/screens/maps/MapsScreen.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {CustomMap} from '../../components/maps/Map';
+// import {CustomMap} from '../../components/maps/Map';
 
 export const MapsScreen = () => {
-  return (
-    <View style={styles.fullContainer}>
-      <CustomMap />
-    </View>
-  );
+  return <View style={styles.fullContainer}>{/* <CustomMap /> */}</View>;
 };
 
 const styles = StyleSheet.create({
